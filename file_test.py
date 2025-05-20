25e1cef19c4c8943f23a445ab01aaf2902eddba0
Remy <remy.canal@live.fr>
2025-05-20 16:20:40 +0000

    Action Control and Test Unit fixed

--- file_test.py
+++ file_test.py
@@ -1,21 +1,22 @@
-import pytest
-
 def test_calc_addition():
-  # Fonction test du résultat de 2+4
+    # Fonction test du résultat de 2+4
     output = 2+4
     assert output == 6
 
+
 def test_calc_substraction():
-  # Fonction test du résultat de 2-4
+    # Fonction test du résultat de 2-4
     output = 2-4
     assert output == -2
 
+
 def test_calc_multiply():
-  # Fonction test du résultat de 2*4
+    # Fonction test du résultat de 2*4
     output = 2*4
     assert output == 8
 
+
 def test_coucou():
-  # Fonction test si la résultat renvoie 'hello'
+    # Fonction test si la résultat renvoie 'hello'
     output='hello'
     assert output == 'hello'
